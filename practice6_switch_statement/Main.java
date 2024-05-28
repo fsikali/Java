ce351ad43eaeb1c3f0f721279132ac9c9f2a2779
fsikali <flemmingsikali499@gmail.com>
2024-05-28 12:47:26 +0300

    Modified: folder ../practice5_for_loop
    Created new file _ folder name: practice6_switch_statement

---
+++ practice6_switch_statement/Main.java
@@ -0,0 +1,32 @@
+// The switch expression is evaluated once 
+// The value of the expression is compared with the value of each case
+// If there is a match the assosited block of code is executed  
+
+public class Main { 
+    public static void main(String[] args) { 
+        int score = 5; 
+        switch (score) { 
+            case 1: 
+            System.out.println("Mathematics");  
+            break; 
+            case 2: 
+            System.out.println("English"); 
+            break; 
+            case 3: 
+            System.out.println("Kiswahili"); 
+            break; 
+            case 4: 
+            System.out.println("Chemistry"); 
+            break; 
+            case 5: 
+            System.out.println("Physics"); 
+            break;
+            case 6: 
+            System.out.println("Geography");
+            break; 
+            case 7: 
+            System.out.println("History"); 
+            break;
+        }
+    }
+}
