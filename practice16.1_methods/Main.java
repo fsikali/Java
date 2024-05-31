b1a5c6925bd6c2d6a38c612f07dae53f2e2b5708
fsikali <flemmingsikali499@gmail.com>
2024-05-31 15:47:19 +0300

    Added new file: folder practice16.1

---
+++ practice16.1_methods/Main.java
@@ -0,0 +1,18 @@
+// Methods, Parameters and Arguments
+// Parameters are specified after the method name inside the parentheses 
+// ? Check how to use methods 
+
+public class Main { 
+    public static void main(String[] args) { 
+        checkName("Flemming ", "Sikali"); 
+        getNumber(10);
+    } 
+
+    static void checkName(String firstName, String lastName) { 
+        System.out.println("Your name is: " + firstName + lastName);
+    } 
+
+    static void getNumber(int x) { 
+        System.out.println("This is your number: " + x );
+    }  
+}
---
+++ practice16.2_methods/Main.java
@@ -0,0 +1,12 @@
+// Return types in methods 
+
+public class Main { 
+    public static void main(String[] args) { 
+        int z = addNumbers(10, 20);
+        System.out.println("This is the sum: " + z); 
+    } 
+
+    static int addNumbers(int x, int y) {  
+        return (x + y);
+    }
+}
