66c8c151c6d97e3005974b6fbb3627bdecc9def8
fsikali <flemmingsikali499@gmail.com>
2024-06-01 18:28:38 +0300

    Added two new files: Main.java and Main.class

---
+++ practice17_variables/Main.java
@@ -0,0 +1,20 @@
+// Variables in Java
+
+public class Main { 
+    public static void main(String[] args) { 
+        int x = 10; 
+        System.out.println("The value of x is: " + x); 
+
+        x = 15;
+        
+         {  
+              
+           x = x * 2;   
+           System.out.println("The value of x is: " + x); 
+        } 
+
+
+           System.out.println("The value of x is: " + x);
+
+    }
+}
