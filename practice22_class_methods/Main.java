73a738e2af27092b5315a8ea9cedc451f7e5096e
fsikali <flemmingsikali499@gmail.com>
2024-07-16 18:24:26 +0300

    Modified: README.md

---
+++ practice22_class_methods/Main.java
@@ -0,0 +1,52 @@
+// Methods are declared within a class and are used to perform certain
+// actions 
+
+// Create a method named calculations 
+
+public class Main { 
+    public static void main(String[] args) { 
+
+        int finalScore = calculations(10, 20); 
+        System.out.println("This is my final score: " + finalScore); 
+    } 
+
+    static int calculations(int score_a, int score_b) {  
+
+        return (score_a + score_b);  
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
