7333d3608630b5741f1f3cb064f6e53d9ea29441
fsikali <flemmingsikali499@gmail.com>
2024-08-21 19:50:40 +0300

    modified: practice3_while_loop && practice5_for_loop && practice6_switch_statement && practice9_break_keyword

--- practice9_break_keyword/Main.java
+++ practice9_break_keyword/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) { 
         int score = 45; 
         while(score <= 100) { 
-            System.out.println("Your score is: "+ score); 
+          //  System.out.println("Your score is: "+ score); 
             score++; 
 
             if (score == 65){ 
