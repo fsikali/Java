93f810889d4ea15ef55c3d49310f62343a6af944
fsikali <flemmingsikali499@gmail.com>
2024-09-02 19:24:37 +0300

    new file: methods_e/Main.class && methods_e/Main.java

---
+++ methods_e/Main.java
@@ -0,0 +1,21 @@
+public class Main { 
+    public static void main(String[] args) {
+        System.out.println("Hi Flemming");   
+
+        myScore(10); 
+        
+        int John = 5; 
+        int Peter = 20; 
+
+        myScore(John); 
+        myScore(Peter);
+
+    }  
+  
+
+    static void myScore(int score) {  
+          System.out.println("My score is: " + score); 
+          
+    }
+} 
+
