f3913ca3d2a79c03bb32b8c83dc284846e21f0c7
fsikali <flemmingsikali499@gmail.com>
2025-01-10 20:53:37 +0300

    modified: accessing_class_methods/Main.java, accessing_class_methods_a/Main.java, accessing_class_methods_a/Main.java

--- accessing_class_methods_a/Main.java
+++ accessing_class_methods_a/Main.java
@@ -1,25 +1,34 @@
 public class Main {
     public void gamePlayed() {
         System.out.println("I played basketball"); 
-    } 
+    }  
+
+    public void playerName(String name) { 
+        System.out.println("The player name is: " + name);
+    }
 
     public void score(int maxScore) { 
         System.out.println("Max score in the game is: " + maxScore); 
 
     } 
 
+    public void anotherScore(int minScore) { 
+        System.out.println("Min score in the game is: " + minScore);
+    }
+    
+    static void leaderTeam(String name) { 
+        System.out.println("The leading team is: " + name);
+    }
     public static void main(String[] args) { 
         Main basketball = new Main(); 
         basketball.gamePlayed(); 
+        basketball.playerName("Flemming");
         basketball.score(250);
+        basketball.anotherScore(100);
+
+        leaderTeam("Team Simba");
     }
 } 
 
  
   
-
-
-
-
-   
-     
