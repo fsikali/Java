18c29f8de5bbdc5729a113e9cd1b47c467d748c7
fsikali <flemmingsikali499@gmail.com>
2025-01-31 13:53:21 +0300

    modified: Main.class, Main.java, Simba.class, Team.class

--- objects_a/Main.java
+++ objects_a/Main.java
@@ -8,20 +8,39 @@
 class Team {  
     public int score; 
     public int anotherScore; 
+    public String motto; 
 }   
 
 class Simba extends Team { 
 
-    public Simba(int firstScore, int secondScore) {
-         score = firstScore;
-         anotherScore = secondScore;
-    }
-}
+    public int highScore;
+    
+        public Simba(int firstScore, int secondScore) {
+             score = firstScore;
+             anotherScore = secondScore; 
+        } 
+    
+        public void teamMotto() {  
+            String motto = "In God we trust";
+            System.out.println("Team Simba motto: " + motto);
+        } 
 
-public class Main { 
-    public static void main(String [] args) { 
-        Simba teamSimba = new Simba(90, 100);   
-        System.out.println("Team Simba scored: " + teamSimba.score + " and " + teamSimba.anotherScore);
+        public void teamResults() { 
+            System.out.println("This was the first score: " + score); 
+            System.out.println("This was the second score: " + anotherScore);
+        }
+    }
+    
+    public class Main { 
+        public static void main(String [] args) { 
+            Simba teamSimba = new Simba(90, 100);  
+            teamSimba.highScore = 100;
+            teamSimba.teamMotto();  
+            teamSimba.teamResults();
+             
+
+            System.out.println("Team Simba scored: " + teamSimba.score + " and " + teamSimba.anotherScore);
+            System.out.println("The highest score in the match was: " + teamSimba.highScore);
     }
 }
 
