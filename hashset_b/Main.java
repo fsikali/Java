b0342866fa262c7b1d8e34ae49779431e83ade6b
fsikali <flemmingsikali499@gmail.com>
2025-02-22 09:24:16 +0300

    modified: hashset_b/Main.java

--- hashset_b/Main.java
+++ hashset_b/Main.java
@@ -2,3 +2,19 @@
 --- Check If an Item Exists
 - To check whether an item exists in a HashSet, use the contains() method:
 */
+
+import java.util.HashSet;
+
+public class Main { 
+    public static void main(String[] args) {
+        HashSet<String> cars = new HashSet<String>();
+
+        cars.add("Volvo");
+        cars.add("BMW"); 
+        cars.add("Ford"); 
+        cars.add("BMW");
+        cars.add("Mazda"); 
+        cars.contains("Mazda"); 
+        System.out.println(cars.contains("Mazda"));
+    }
+} 
